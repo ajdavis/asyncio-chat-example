5beb031568ce448f328c74afa8eb0f2cff503548
A. Jesse Jiryu Davis <jesse@mongodb.com>
2014-03-28 14:04:54 -0400

    Remove Eliza. Make it a multi-user chat server.

--- server.py
+++ server.py
@@ -1,31 +1,31 @@
 import asyncio
-import json
-import traceback
+import logging
 
+# https://pypi.python.org/pypi/websockets
 import websockets
 
-import eliza
+clients = set()
+logging.basicConfig(level=logging.INFO)
 
 
 @asyncio.coroutine
 def chat(websocket, uri):
-    try:
-        print('open')
-        while True:
-            body = yield from websocket.recv()
-            if body is None:
-                print('close')
-                return
+    print('open')
+    clients.add(websocket)
+    while True:
+        msg = yield from websocket.recv()
+        if msg is None:
+            print('close')
+            return
 
-            print(body)
-            msg = json.loads(body).get('msg')
-            response = {'msg': eliza.respond(msg)}
-            yield from websocket.send(json.dumps(response))
-    except Exception:
-        traceback.print_exc()
-        return
+        print(msg)
+        for client in clients.copy():
+            if client is not websocket:
+                try:
+                    yield from client.send(msg)
+                except websockets.exceptions.InvalidState:
+                    clients.remove(client)
 
 start_server = websockets.serve(chat, 'localhost', 8765)
-
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
